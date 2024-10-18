22a1ff0c77bd283834a388be6fb66a7ad6e16c71
BrianKvin <briankvin@gmail.com>
2024-10-18 09:38:59 +0300

    Find the longest word in an argument

---
+++ longest_word.js
@@ -0,0 +1,14 @@
+// A function that takes a string as an argument and returns its longest word
+
+function longestWord(str) {
+  let words = str.split(" ");
+  let longestWord = "";
+
+  for (let word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  return longestWord;
+}
+console.log(longestWord("I will be a good engineer"));
