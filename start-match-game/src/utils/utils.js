48d917cbd5e4732d6ed7a1a4c67c4a6aeb8262cb
BrianKvin <briankvin@gmail.com>
2025-09-18 15:45:31 +0300

    game logic

--- start-match-game/src/utils/utils.js
+++ start-match-game/src/utils/utils.js
@@ -5,7 +5,7 @@ const utils = {
 
   random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
 
-  randomSunIn: (arr, max) => {
+  randomSumIn: (arr, max) => {
     const sets = [[]];
     const sums = [];
 
